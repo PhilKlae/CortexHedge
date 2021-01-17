6e3817e8c4f61cbf09258b55a2be1776edd97890
SchickMarkus <schickmarkus@web.de>
2021-01-17 22:59:49 +0100

    Setup deployment environment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,58 @@
 require("@nomiclabs/hardhat-waffle");
-
-
+require('dotenv').config();
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+// Go to https://infura.io/ and create a new project
+// Replace this with your Infura project ID
+const infuraProjectId = process.env.INFURA_ID;
+const mnemonic = process.env.DEV_MNEMONIC;
+
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+
 module.exports = {
-  solidity: "0.7.3",
-};
+  defaultNetwork: "kovan",
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${infuraProjectId}`,
+      accounts: {mnemonic: mnemonic}
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${infuraProjectId}`,
+      accounts: {mnemonic: mnemonic}
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${infuraProjectId}`,
+      accounts: {mnemonic: mnemonic}
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${infuraProjectId}`,
+      accounts: {mnemonic: mnemonic}
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${infuraProjectId}`,
+      accounts: {mnemonic: mnemonic}
+    }
+  },
+  solidity: {
+    version: "0.7.3",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  }
+}
