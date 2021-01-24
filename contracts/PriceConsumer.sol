af43710f0b3af8ddaf5e019771d770ada4a457df
SchickMarkus <schickmarkus@web.de>
2021-01-24 21:19:56 +0100

    Add Chainlink oracles in seperate contract

---
+++ contracts/PriceConsumer.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.7.3;
+
+import "@chainlink/contracts/src/v0.7/interfaces/AggregatorV3Interface.sol";
+
+
+abstract contract PriceConsumer {
+
+    /*
+     * Network: Mainnet
+     * Aggregator: ETH/USD
+     * Address: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+     */
+    AggregatorV3Interface internal priceFeed_eur_usd;
+    AggregatorV3Interface internal priceFeed_dai_usd;
+
+
+    function getEUROPrice() public view returns (int) {
+        (
+            uint80 roundID, 
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed_eur_usd.latestRoundData();
+        // If the round is not complete yet, timestamp is 0
+        require(timeStamp > 0, "Round not complete");
+        return price;
+    }
+
+    function getDAIPrice() public view returns (int) {
+        (
+            uint80 roundID, 
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed_dai_usd.latestRoundData();
+        // If the round is not complete yet, timestamp is 0
+        require(timeStamp > 0, "Round not complete");
+        return price;
+    }
+}
+   
