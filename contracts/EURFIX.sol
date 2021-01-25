bfcfd1a5268d9e0691ae18e790451160e1c9eddc
SchickMarkus <schickmarkus@web.de>
2021-01-25 12:30:19 +0100

    Change solidity version in derivative tokens

--- contracts/EURFIX.sol
+++ contracts/EURFIX.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.2;
+pragma solidity ^0.7.3;
 
-import '@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol';
+import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
 
 contract EURFIX is ERC20PresetMinterPauser {
-    constructor(address minter_address) public ERC20PresetMinterPauser("EURFIX", "EURFIX") {
+    constructor(address minter_address)
+        public
+        ERC20PresetMinterPauser("EURFIX", "EURFIX")
+    {
         _setupRole(MINTER_ROLE, minter_address);
     }
 }
--- contracts/USDFLOAT.sol
+++ contracts/USDFLOAT.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.2;
+pragma solidity ^0.7.3;
 
-import '@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol';
+import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
 
 contract USDFLOAT is ERC20PresetMinterPauser {
-    constructor(address minter_address) public ERC20PresetMinterPauser("USDFLOAT", "USDFLOAT") {   
+    constructor(address minter_address)
+        public
+        ERC20PresetMinterPauser("USDFLOAT", "USDFLOAT")
+    {
         _setupRole(MINTER_ROLE, minter_address);
     }
 }
