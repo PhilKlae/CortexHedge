ef31cac95cdb56876c94d5fd69b158ab8d9429ef
SchickMarkus <schickmarkus@web.de>
2021-01-30 21:37:34 +0100

    Add further functionality (multiple solidity versions, use alchemy api

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,21 @@ require('dotenv').config();
  */
 // Go to https://infura.io/ and create a new project
 // Replace this with your Infura project ID
+
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners()
+
+  for (const account of accounts) {
+    console.log(account.address)
+  }
+})
+
 const infuraProjectId = process.env.INFURA_ID;
 const mnemonic = process.env.DEV_MNEMONIC;
+const alchemyAPI = process.env.ALCHEMY_API;
 
 // Replace this private key with your Ropsten account private key
 // To export your private key from Metamask, open Metamask and
@@ -18,7 +31,9 @@ module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url: `https://mainnet.infura.io/v3/${infuraProjectId}`,
+        // fix block number to speed up forkings
+        url: `https://eth-mainnet.alchemyapi.io/v2/${alchemyAPI}`,
+        blockNumber: 11758657 
       }
     },
     rinkeby: {
@@ -36,20 +51,24 @@ module.exports = {
     goerli: {
       url: `https://goerli.infura.io/v3/${infuraProjectId}`,
       accounts: {mnemonic: mnemonic}
-    },
-    mainnet: {
-      url: `https://mainnet.infura.io/v3/${infuraProjectId}`,
-      accounts: {mnemonic: mnemonic}
     }
   },
   solidity: {
-    version: "0.7.3",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
+    compilers: [
+      {
+      version: "0.7.3",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        },
       }
-    }
+      },
+      {
+        version: "0.6.6",
+        settings: { } 
+      }
+    ]
   },
   paths: {
     sources: "./contracts",
