f827b2b9bcf131da9922cc26933a33dbded1bc1d
SchickMarkus <schickmarkus@web.de>
2021-02-01 17:22:02 +0100

    Use big numbers for test values

--- test/SwapContract.js
+++ test/SwapContract.js
@@ -1,5 +1,5 @@
 const { expect } = require("chai");
-const { waffle } = require("hardhat")
+const { waffle } = require("hardhat");
 
 describe("Swap Contract", function () {
   let SwapContract;
@@ -17,7 +17,11 @@ describe("Swap Contract", function () {
   let redeemer;
   let addrs;
 
-  let totalDAISupply = 5000;
+  // test data. Use BigNumber to avoid overflow
+  const totalDAISupply = ethers.BigNumber.from("1000000000000000000000");
+  const initialContractBalance = ethers.BigNumber.from("100000000000000000000");
+  const ownerDAISupply = totalDAISupply.sub(initialContractBalance);
+  const approvedAmount = ethers.BigNumber.from("100000000000000000000");
 
   beforeEach(async () => {
 
@@ -47,16 +51,21 @@ describe("Swap Contract", function () {
     await hardhatSwapContract.set_USDFLOAT_address(hardhatUSDFLOAT.address);
     await hardhatSwapContract.set_Dai_address(hardhatDAI.address);
 
+    // send initial supply of Dai to the pool
+    await hardhatDAI.transfer(hardhatSwapContract.address, initialContractBalance);
+
   });
 
 
   describe("Savings Period", function () {
     it("Should allow Owner to start the savings period", async function () {
+      // contract call
       await hardhatSwapContract.start_saving();
       const exchange_rate_start = await hardhatSwapContract.exchange_rate_start();
+      
+      // should be sucessfull
       expect(exchange_rate_start).not.be.null;
       expect(exchange_rate_start).to.equal(await hardhatSwapContract.getEUROPrice());
-
       const principal_balance = await hardhatSwapContract.total_pool_prinicipal();
       console.log(
         "Swap Contract can spend up to ",
@@ -74,29 +83,36 @@ describe("Swap Contract", function () {
     it("Should give the deployer of DAI some tokens", async function() {
       // Deployer address should receive the MINTER_ROLE
       const ownerBalance = await hardhatDAI.balanceOf(owner.address);
-      expect(await hardhatDAI.totalSupply()).to.equal(ownerBalance);
+      expect(await ownerDAISupply).to.equal(ownerBalance);
     });
     it("Should give give Swap Contract allowance to spend dai", async function () {
-      const DAI_amount = 500;
-      const is_approved = await hardhatDAI.approve(hardhatSwapContract.address, DAI_amount);
-      //expect(is_approved).to.be.true;
+
+      // give approval 
+      await hardhatDAI.approve(hardhatSwapContract.address, approvedAmount);
+
+      // check allowance
       const SwapContractAllowance = await hardhatDAI.allowance(owner.address, hardhatSwapContract.address)
       console.log(
         "Swap Contract can spend up to ",
         ethers.utils.formatEther(SwapContractAllowance),
         "Dai"
       )
-      expect(SwapContractAllowance).to.be.equal(DAI_amount);
+      expect(SwapContractAllowance).to.be.equal(approvedAmount);
     });
 
     it("Should exchange dai for both certificates", async function () {
-      const DAI_amount = 500;
+
       // send some initial Dai to the contract
-      await hardhatDAI.transfer(hardhatSwapContract.address, 500);
       await hardhatSwapContract.start_saving();
       // start minting coins 
-      await hardhatDAI.approve(hardhatSwapContract.address, DAI_amount);
-      await hardhatSwapContract.invest(DAI_amount);
+      await hardhatDAI.approve(hardhatSwapContract.address, approvedAmount);
+      await hardhatSwapContract.invest(approvedAmount);
+      console.log(
+        "Owner has ", 
+        ethers.utils.formatEther(await hardhatEURFIX.balanceOf(owner.address)),
+        "EURFIX and ", 
+        ethers.utils.formatEther(await hardhatUSDFLOAT.balanceOf(owner.address)), 
+        "USDFLOAT");
     });
   });
 
