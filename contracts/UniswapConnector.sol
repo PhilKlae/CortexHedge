b928ff9f037ddcfc77bc300db2a68032543a1fe4
kipit <kipit@42l.fr>
2021-02-03 18:39:22 +0100

    uniswap connector problem

--- contracts/UniswapConnector.sol
+++ contracts/UniswapConnector.sol
@@ -2,43 +2,59 @@ pragma solidity ^0.6.6;
 
 // import './interfaces/ILiquidityValueCalculator.sol';
 import '@uniswap/v2-periphery/contracts/libraries/UniswapV2Library.sol';
+import '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
 import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
 import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
 
-interface ILiquidityValueCalculator {
-    function computeLiquidityShareValue(uint liquidity, address tokenA, address tokenB) external returns (uint tokenAAmount, uint tokenBAmount);
-}
-
-contract UniswapConnector is ILiquidityValueCalculator {
+contract UniswapConnector {
     address public factory;
+
     constructor(address factory_) public {
         factory = factory_;
     }
 
-    function pairInfo(address tokenA, address tokenB) internal view returns (uint reserveA, uint reserveB, uint totalSupply) {
+    function pairInfo(address tokenA, address tokenB)
+        public
+        view
+        returns (uint reserveA, uint reserveB, uint totalSupply)
+    {
         IUniswapV2Pair pair = IUniswapV2Pair(UniswapV2Library.pairFor(factory, tokenA, tokenB));
-        if (pair == address(0)) {
-            IUniswapV2Factory(factory).createPair(tokenA, tokenB);
-        }
+        // if (pair == address(0)) {
+        //     IUniswapV2Factory(factory).createPair(tokenA, tokenB);
+        // }
         totalSupply = pair.totalSupply();
         (uint reserves0, uint reserves1,) = pair.getReserves();
         (reserveA, reserveB) = tokenA == pair.token0() ? (reserves0, reserves1) : (reserves1, reserves0);
     }
 
-    function computeLiquidityShareValue(uint liquidity, address tokenA, address tokenB) external override returns (uint tokenAAmount, uint tokenBAmount) {
-        revert('TODO');
+    // function getInfo() public returns (address facto) {
+    //     facto = IUniswapV2Router02.factory();
+    // }
+
+    function computeLiquidityShareValue(
+        uint liquidity,
+        address tokenA,
+        address tokenB
+    )
+        external
+        returns (uint tokenAAmount, uint tokenBAmount)
+    {
+        (uint reserveA, uint reserveB, uint totalSupply) = pairInfo(tokenA, tokenB);
+        uint tokenAAmount = reserveA;
+        uint tokenBAmount = reserveB;
+        // revert('TODO');
     }
 
     // I need to retrieve the price of DAI/ETH before making the swap, to preevent front running
-    function swapToken (uint _amount) {
-        uint amountIn = _amount * 10 ** DAI.decimals();
-        require(DAI.transferFrom(msg.sender, address(this), amountIn), 'transferFrom failed.');
-        require(DAI.approve(address(UniswapV2Router02), amountIn), 'approve failed.');
+    // function swapToken (uint _amount) {
+    //     uint amountIn = _amount * 10 ** DAI.decimals();
+    //     require(DAI.transferFrom(msg.sender, address(this), amountIn), 'transferFrom failed.');
+    //     require(DAI.approve(address(UniswapV2Router02), amountIn), 'approve failed.');
 
-        // amountOutMin must be retrieved from an oracle of some kind
-        address[] memory path = new address[](2);
-        path[0] = address(DAI);
-        path[1] = UniswapV2Router02.WETH();
-        UniswapV2Router02.swapExactTokensForETH(amountIn, amountOutMin, path, msg.sender, block.timestamp);
-    }
+    //     // amountOutMin must be retrieved from an oracle of some kind
+    //     address[] memory path = new address[](2);
+    //     path[0] = address(DAI);
+    //     path[1] = UniswapV2Router02.WETH();
+    //     UniswapV2Router02.swapExactTokensForETH(amountIn, amountOutMin, path, msg.sender, block.timestamp);
+    // }
 }
---
+++ test/UniswapConnector.js
@@ -0,0 +1,39 @@
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+const contract = require('@truffle/contract');
+const FactoryBytecode = require('@uniswap/v2-core/build/UniswapV2Factory.json')
+const UniswapV2Factory = contract(FactoryBytecode);
+
+describe("Uniswap connector contract", function() {
+  it("Should get the reserve of the pool DAI/WETH and the total supply", async function() {
+    const [owner] = await ethers.getSigners();
+
+
+    const uniswapFactoryKovan = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+    const wethKovan = '0xd0A1E359811322d97991E03f863a0C30C2cF029C'
+    const daiKovan = '0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa'
+
+    const Factory = await ethers.getContractAt(FactoryBytecode)
+    const hardhatFactory = await Factory.deploy()
+
+    console.log(hardhatFactory)
+
+    const Uniswap = await ethers.getContractFactory("UniswapConnector");
+    const hardhatUniswap = await Uniswap.deploy(uniswapFactoryKovan);
+
+    // console.log('hello', hardhatUniswap)
+
+    // const factory = await hardhatUniswap.getInfo()
+    // console.log(factory)
+    // const result =  await hardhatUniswap.computeLiquidityShareValue(
+    //   100,
+    //   wethKovan,
+    //   daiKovan
+    // )
+
+    console.log(result)
+    // const ownerBalance = await hardhatToken.balanceOf(owner.address);
+    // expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+  });
+});
+
