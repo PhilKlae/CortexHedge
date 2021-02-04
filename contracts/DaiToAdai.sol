14c965f65028f194ce281276f816e65534ea6943
PhilKlae <philipp.klaeser@t-online.de>
2021-02-04 20:29:38 +0100

    Try to deposit DAI

--- contracts/DaiToAdai.sol
+++ contracts/DaiToAdai.sol
@@ -25,15 +25,11 @@ contract DaiToAdai {
     // Lendingpool adress provider kovan 0x88757f2f99175387ab4c6a4b3067c77a695b0349
     //get lending pool adress
 
-    //  ADAI public s; // kovan 0x3ae4c2a436f1e5f995310179be2e838ab5cfa4c3
-    
-    // 0x6b175474e89094c44da98b954eedeac495271d0f
+    //  ADAI public s; // kovan 0x3ae4c2a436f1e5f995310179be2e838ab5cfa4c3        
 
-    address constant public DAI_Adress = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address constant public aDAI_Address = 0xfC1E690f61EFd961294b3e1Ce3313fBD8aa4f85d;
-
-   //0x6b175474e89094c44da98b954eedeac495271d0f;
-
+    address constant public DAI_Adress = 0x6B175474E89094C44Da98b954EedeAC495271d0F; //mainnet
+    address constant public aDAI_Address = 0xfC1E690f61EFd961294b3e1Ce3313fBD8aa4f85d; //mainnet
+  
     //approveDelegation()
     /**
      * Contract initialization.
@@ -58,18 +54,14 @@ contract DaiToAdai {
 
         console.log("created lending pool");
         return address(lendingPool);
-
     }
 
     function deposit(
         address pool,
         address token, 
         uint256 amount
-    ) public {
-        
-
+    ) public {        
         ILendingPool(pool).deposit(token, amount, msg.sender, 0);
-
     }
 
     function withdraw(
@@ -77,9 +69,7 @@ contract DaiToAdai {
         address token,        
         uint256 amount
     ) public {
-
         ILendingPool(pool).withdraw(token,amount,msg.sender);
-
     }
 
     function addAllowance(
--- test/AaveIntegration/DaiToAave.js
+++ test/AaveIntegration/DaiToAave.js
@@ -9,6 +9,23 @@ describe("DaiToAave functions", function () {
     let minter;
     let redeemer;
     let addrs;
+    let DaiToAveFactory;
+    let daiToAave;
+
+    const whale = '0x04ad0703b9c14a85a02920964f389973e094e153';
+
+
+    const impersonateAddress = async (address) => {
+      await hre.network.provider.request({
+        method: "hardhat_impersonateAccount",
+        params: [address],
+      });
+    
+      const signer = await ethers.provider.getSigner(address);
+      signer.address = signer._address;
+      
+      return signer;
+    }
 
     beforeEach(async () => {
   
@@ -18,6 +35,12 @@ describe("DaiToAave functions", function () {
     beforeEach('Set up accounts', async () => {
         // get addresses to interact
         [owner, minter, redeemer, ...addrs] = await ethers.getSigners();
+
+        DaiToAveFactory = await ethers.getContractFactory("DaiToAdai");
+    
+        daiToAave = await DaiToAveFactory.deploy();
+        
+        
     });
 
     /*
@@ -27,22 +50,67 @@ describe("DaiToAave functions", function () {
       });
     });
     */
-    describe("Get LendingPoolAdress", function () {
-      it("GetLendingPoolAdress ", async function () {
+    describe("Basics ", function () {
+      
+      it("GetLendingPoolAdress ", async function () {        
 
-        const [owner] = await ethers.getSigners();
-
-        const Token = await ethers.getContractFactory("DaiToAdai");
-    
-        const hardhatToken = await Token.deploy();
-        
-        address = await hardhatToken.connect(minter).GetLendingPoolAdress();
+        address = await daiToAave.connect(minter).GetLendingPoolAdress();
 
         console.log(address);
 
       });
-    });
-  
 
-  
+      it("Transfer Dai ", async function () {
+
+        let transferAmount = 5000;            
+        balance = await OccupyDAI(owner,transferAmount);
+        expect(balance).to.equal(ethers.utils.parseEther(""+transferAmount) );
+
+   
+      });
+
+
+      it("Lend Dai ", async function () {
+        
+        let DaiAmount = 5000;    
+        balance = await OccupyDAI(owner,DaiAmount);
+        let lendAmount = 2500;
+
+        ownerAave = daiToAave.connect(owner);
+
+        pool = await ownerAave.GetLendingPoolAdress();
+        //let principal = await ownerAave.GetPrincipalAmount();
+        //console.log(principal);
+        await ownerAave.deposit(pool,'0x6B175474E89094C44Da98b954EedeAC495271d0F',ethers.utils.parseEther(""+lendAmount));
+
+      });
+      
+    });
+
+    async function OccupyDAI( new_owner , transferAmount){
+
+      let dai = await ethers.getContractAt('@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20', '0x6b175474e89094c44da98b954eedeac495271d0f');                
+
+      const whaleSigner = await impersonateAddress(whale);        
+
+      let balance = await dai.balanceOf(new_owner.address);
+      /*console.log(
+        "our very own balance (before) ",
+        ethers.utils.formatEther(balance)
+      ) */       
+      
+      dai = dai.connect(whaleSigner);                
+      
+      await dai.transfer(new_owner.address, ethers.utils.parseEther(""+transferAmount));            
+
+      balance = await dai.balanceOf(new_owner.address);
+
+      return balance;
+      /*console.log(
+        "our very own balance (after) ",
+        ethers.utils.formatEther(balance)
+      )   */    
+
+    }
+
   });
