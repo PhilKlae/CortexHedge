611b2a2836fc172363803e396ce5836fffd3ded3
PhilKlae <philipp.klaeser@t-online.de>
2021-02-07 14:45:31 +0100

    push missing files

---
+++ scripts/2_deploy_swap_module.js
@@ -0,0 +1,35 @@
+export async function DeploySwapper( addresses ) {
+
+    const [deployer] = await ethers.getSigners();
+  
+    console.log(
+      "Deploying contracts with the account:",
+      deployer.address
+    );
+    
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+  
+    // main swap contract
+    const SwapContract = await ethers.getContractFactory("SwapContract");
+    const hardhatSwapContract = await SwapContract.connect(owner).deploy();
+    await hardhatSwapContract.deployed();
+
+    // launch auxillary tokens and connect to main contract
+    const EURFIX = await ethers.getContractFactory("EURFIX");
+    const hardhatEURFIX = await EURFIX.connect(minter).deploy(hardhatSwapContract.address);
+    await hardhatEURFIX.deployed();
+
+    const USDFLOAT = await ethers.getContractFactory("USDFLOAT");
+    const hardhatUSDFLOAT = await USDFLOAT.connect(redeemer).deploy(hardhatSwapContract.address);
+    await hardhatUSDFLOAT.deployed();
+
+    const DAI = await ethers.getContractFactory("DAI");
+    const hardhatDAI = await DAI.connect(owner).deploy(totalDAISupply);
+    await hardhatDAI.deployed();
+
+    // give derivative contract address to main address
+    await hardhatSwapContract.set_EURFIX_address(hardhatEURFIX.address);
+    await hardhatSwapContract.set_USDFLOAT_address(hardhatUSDFLOAT.address);
+    await hardhatSwapContract.set_Dai_address(hardhatDAI.address);
+  }
+  
--- scripts/deployAll.js
+++ scripts/deployAll.js
@@ -2,6 +2,9 @@ const { expect } = require("ethers");
 const { hexStripZeros } = require("ethers/lib/utils");
 const hre = require("hardhat");
 
+import { DeployCurve } from './1_deploy_curve_module';
+import { DeploySwapper } from './2_deploy_swap_module';
+
 async function main() {
   accounts = await ethers.getSigners()
   let owner = accounts[0];
@@ -11,13 +14,11 @@ async function main() {
 
   let addressDict = {};
 
-
-
   if (hre.network.name == "hardhat") { //mainnet fork or local net 
 
     //Mainnet
-    addressDict["DAIaddress"] = 0x6b175474e89094c44da98b954eedeac495271d0f;
-    addressDict["aDAIaddress"] = 0xfc1e690f61efd961294b3e1ce3313fbd8aa4f85d;
+    addressDict["DAI"] = 0x6b175474e89094c44da98b954eedeac495271d0f;
+    addressDict["aDAI"] = 0xfc1e690f61efd961294b3e1ce3313fbd8aa4f85d;
     addressDict["AaveLendingPoolProvider"] = 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5;
     addressDict["Chainlink"] = 0xb49f677943BC038e9857d61E7d053CaA2C1734C1;
     addressDict["UniFactory"] = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;//v2
@@ -28,8 +29,8 @@ async function main() {
   } else if (hre.network.name == "Kovan") {
 
     //KovanAddresses
-    addressDict["DAIaddress"] = 0xff795577d9ac8bd7d90ee22b6c1703490b6512fd;
-    addressDict["aDAIaddress"] = 0xdcf0af9e59c002fa3aa091a46196b37530fd48a8;
+    addressDict["DAI"] = 0xff795577d9ac8bd7d90ee22b6c1703490b6512fd;
+    addressDict["aDAI"] = 0xdcf0af9e59c002fa3aa091a46196b37530fd48a8;
     addressDict["AaveLendingPoolProvider"] = 0x88757f2f99175387ab4c6a4b3067c77a695b0349;
     addressDict["Chainlink"] = 0x0c15Ab9A0DB086e062194c273CC79f41597Bbf13;//0xb49f677943BC038e9857d61E7d053CaA2C1734C1 mainnet
     addressDict["UniFactory"] = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
@@ -38,6 +39,12 @@ async function main() {
 
   }
 
+  await DeployCurve(addressDict);
+
+  await DeploySwapper(addressDict);
+  
+
+  
   //deploy eurs
   //deploy seur
 
