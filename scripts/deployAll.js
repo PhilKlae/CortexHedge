bb212b92bbbe963739c2f84ba09e7786881914e0
PhilKlae <philipp.klaeser@t-online.de>
2021-02-07 20:26:34 +0100

    correctly export deployment, add new test file for full test

--- scripts/deployAll.js
+++ scripts/deployAll.js
@@ -1,3 +1,5 @@
+
+
 const { expect } = require("ethers");
 //const { hexStripZeros } = require("ethers/lib/utils");
 const hre = require("hardhat");
@@ -5,8 +7,7 @@ const routerJson = require('@uniswap/v2-periphery/build/UniswapV2Router02.json')
 const ether = require("@openzeppelin/test-helpers/src/ether");
 
 
-
-async function main() {
+   async function main() {
   accounts = await ethers.getSigners()
   let owner = accounts[0];
   let launcher2 = accounts[1];
@@ -36,7 +37,7 @@ async function main() {
     addressDict["aDAI"] = "0xdcf0af9e59c002fa3aa091a46196b37530fd48a8";
     addressDict["AaveLendingPoolProvider"] = "0x88757f2f99175387ab4c6a4b3067c77a695b0349";
     addressDict["Chainlink"] = "0x0c15Ab9A0DB086e062194c273CC79f41597Bbf13";//0xb49f677943BC038e9857d61E7d053CaA2C1734C1 mainnet
-    addressDict["UniFactory"] = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"; 
+    addressDict["UniFactory"] = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
     addressDict["UniRouter"] = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
     addressDict["Weth"] = "0xd0A1E359811322d97991E03f863a0C30C2cF029C";
 
@@ -55,32 +56,34 @@ async function main() {
   await DeployUniswap(addressDict, owner);
 
   //deploys swapper 
-  await DeploySwapper(addressDict, owner,launcher2, launcher3);
+  await DeploySwapper(addressDict, owner, launcher2, launcher3);
 
   return addressDict;
 }
 
-async function DeployCurve( addresses, owner ) {
-  
+
+
+async function DeployCurve(addresses, owner) {
+
   console.log(
     "Deploying contracts with the account:",
     owner.address
   );
-  
+
   console.log("Account balance:", (await owner.getBalance()).toString());
 
   const { BN } = require('@openzeppelin/test-helpers');
-  
+
   const ERC20 = artifacts.require('Stub_ERC20');
   const YERC20 = artifacts.require('Stub_YERC20');
-  
+
   const CurveDeposit = artifacts.require('Stub_CurveFi_DepositY');
   const CurveSwap = artifacts.require('Stub_CurveFi_SwapY');
   const CurveLPToken = artifacts.require('Stub_CurveFi_LPTokenY');
   const CurveCRVMinter = artifacts.require('Stub_CurveFi_Minter');
   const CurveGauge = artifacts.require('Stub_CurveFi_Gauge');
   const MoneyToCurve = artifacts.require('MoneyToCurve');
-  
+
   const supplies = {
     sEUR: ethers.utils.parseUnits("1000000", unit = 2),
     EURS: ethers.utils.parseUnits("1000000", unit = 18)
@@ -90,20 +93,20 @@ async function DeployCurve( addresses, owner ) {
   const sEUR = await ERC20.new({ from: owner.address });
   await sEUR.methods['initialize(string,string,uint8,uint256)']('sEUR', 'sEUR', 18, supplies.sEUR, { from: owner.address });
   console.log(
-    "sEUR address", 
-    sEUR.address, 
+    "sEUR address",
+    sEUR.address,
     "@dev: use in UNISWAP pools \n"
   )
-  
+
   addresses["sEUR"] = sEUR.address;
 
   const EURS = await ERC20.new({ from: owner.address });
   await EURS.methods['initialize(string,string,uint8,uint256)']('EURS', 'EURS', 2, supplies.EURS, { from: owner.address });
   console.log(
-    "EURS address", 
-    EURS.address, 
+    "EURS address",
+    EURS.address,
     "@dev: use in UNISWAP pools \n"
-  )    
+  )
 
   addresses["EURs"] = EURS.address;
 
@@ -111,35 +114,35 @@ async function DeployCurve( addresses, owner ) {
   const ysEUR = await YERC20.new({ from: owner.address });
   await ysEUR.initialize(sEUR.address, 'ysEUR', 18, { from: owner.address });
   const yEURS = await YERC20.new({ from: owner.address });
-  await yEURS.initialize(EURS.address, 'yEURS', 2,{ from: owner.address });
+  await yEURS.initialize(EURS.address, 'yEURS', 2, { from: owner.address });
 
   //Prepare stubs of Curve.Fi
-  const curveLPToken = await CurveLPToken.new({from: owner.address});
-  await curveLPToken.methods['initialize()']({from: owner.address});
+  const curveLPToken = await CurveLPToken.new({ from: owner.address });
+  await curveLPToken.methods['initialize()']({ from: owner.address });
 
   //addresses["CurveLPToken"] = curveLPToken.address;
 
   const curveSwap = await CurveSwap.new({ from: owner.address });
   await curveSwap.initialize(
-      [ysEUR.address, yEURS.address],
-      [sEUR.address, EURS.address],
-      curveLPToken.address, 10, { from: owner.address });
-  await curveLPToken.addMinter(curveSwap.address, {from: owner.address});
+    [ysEUR.address, yEURS.address],
+    [sEUR.address, EURS.address],
+    curveLPToken.address, 10, { from: owner.address });
+  await curveLPToken.addMinter(curveSwap.address, { from: owner.address });
 
   //addresses["CurveSwapPool"] = curveSwap.address;
 
   const curveDeposit = await CurveDeposit.new({ from: owner.address });
   await curveDeposit.initialize(
-      [ysEUR.address, yEURS.address],
-      [sEUR.address, EURS.address],
-      curveSwap.address, curveLPToken.address, { from: owner.address });
-  await curveLPToken.addMinter(curveDeposit.address, {from: owner.address});
+    [ysEUR.address, yEURS.address],
+    [sEUR.address, EURS.address],
+    curveSwap.address, curveLPToken.address, { from: owner.address });
+  await curveLPToken.addMinter(curveDeposit.address, { from: owner.address });
   console.log(
-    "curveDeposit address", 
-    curveDeposit.address, 
+    "curveDeposit address",
+    curveDeposit.address,
     "@dev: use in investment module \n"
-  )    
-  
+  )
+
   const crvToken = await ERC20.new({ from: owner.address });
   await crvToken.methods['initialize(string,string,uint8,uint256)']('CRV', 'CRV', 18, 0, { from: owner.address });
 
@@ -147,31 +150,31 @@ async function DeployCurve( addresses, owner ) {
   await curveMinter.initialize(crvToken.address, { from: owner.address });
   await crvToken.addMinter(curveMinter.address, { from: owner.address });
   console.log(
-    "curveMinter address", 
-    curveMinter.address, 
+    "curveMinter address",
+    curveMinter.address,
     "@dev: use in investment module \n"
-  )    
+  )
 
   const curveGauge = await CurveGauge.new({ from: owner.address });
-  await curveGauge.initialize(curveLPToken.address, curveMinter.address, {from: owner.address});
+  await curveGauge.initialize(curveLPToken.address, curveMinter.address, { from: owner.address });
   await crvToken.addMinter(curveGauge.address, { from: owner.address });
   console.log(
-    "curveGauge address", 
-    curveGauge.address, 
+    "curveGauge address",
+    curveGauge.address,
     "@dev: use in investment module \n"
-  )    
+  )
 
 
-  moneyToCurve = await MoneyToCurve.new({from: owner.address});
-  await moneyToCurve.initialize({from: owner.address});
-  await moneyToCurve.setup(curveDeposit.address, curveGauge.address, curveMinter.address, {from: owner.address});
+  moneyToCurve = await MoneyToCurve.new({ from: owner.address });
+  await moneyToCurve.initialize({ from: owner.address });
+  await moneyToCurve.setup(curveDeposit.address, curveGauge.address, curveMinter.address, { from: owner.address });
 
   addresses["moneyToCurve"] = moneyToCurve.address;
 
 }
 
-async function DeploySwapper( addresses, owner , launcher2, launcher3) {  
-  
+async function DeploySwapper(addresses, owner, launcher2, launcher3) {
+
   console.log(addresses);
   let checkpoint = 1;
 
@@ -179,105 +182,105 @@ async function DeploySwapper( addresses, owner , launcher2, launcher3) {
 
   // main swap contract
   console.log("checkpoint " + checkpoint);
-  checkpoint ++;
+  checkpoint++;
 
   const SwapContract = await ethers.getContractFactory("SwapContract");
 
   console.log("checkpoint " + checkpoint);
-  checkpoint ++;
+  checkpoint++;
 
   const hardhatSwapContract = await SwapContract.connect(owner).deploy(
-   addresses["moneyToCurve"],
-   addresses["EURs"],
-   addresses["sEUR"],
-   addresses["UniswapConnectorSeur"],
-   addresses["UniswapConnectorEurS"] );
+    addresses["moneyToCurve"],
+    addresses["EURs"],
+    addresses["sEUR"],
+    addresses["UniswapConnectorSeur"],
+    addresses["UniswapConnectorEurS"]);
 
-   console.log("checkpoint " + checkpoint);
-   checkpoint ++;
+  console.log("checkpoint " + checkpoint);
+  checkpoint++;
 
   await hardhatSwapContract.deployed();
 
   console.log("checkpoint " + checkpoint);
-  checkpoint ++;
-  
+  checkpoint++;
+
   addresses["Main"] = hardhatSwapContract.address;
 
   // launch auxillary tokens and connect to main contract
   const EURFIX = await ethers.getContractFactory("EURFIX");
 
   console.log("checkpoint " + checkpoint);
-  checkpoint ++;
+  checkpoint++;
 
   const hardhatEURFIX = await EURFIX.connect(launcher2).deploy(hardhatSwapContract.address);
   await hardhatEURFIX.deployed();
 
   console.log("checkpoint " + checkpoint);
-  checkpoint ++;
-  
+  checkpoint++;
+
   addresses["EURFIX"] = hardhatEURFIX.address;
 
   const USDFLOAT = await ethers.getContractFactory("USDFLOAT");
 
   console.log("checkpoint " + checkpoint);
-  checkpoint ++;
+  checkpoint++;
 
   const hardhatUSDFLOAT = await USDFLOAT.connect(launcher3).deploy(hardhatSwapContract.address);
 
   console.log("checkpoint " + checkpoint);
-  checkpoint ++;
+  checkpoint++;
 
   await hardhatUSDFLOAT.deployed();
 
   console.log("checkpoint " + checkpoint);
-  checkpoint ++;
+  checkpoint++;
 
   addresses["USDFLOAT"] = hardhatUSDFLOAT.address;
 
   // give derivative contract address to main address
   await hardhatSwapContract.set_EURFIX_address(hardhatEURFIX.address);
   console.log("checkpoint " + checkpoint);
-  checkpoint ++;
+  checkpoint++;
   await hardhatSwapContract.set_USDFLOAT_address(hardhatUSDFLOAT.address);
   console.log("checkpoint " + checkpoint);
-  checkpoint ++;
+  checkpoint++;
   await hardhatSwapContract.set_Dai_address(addresses["DAI"]);
   console.log("checkpoint " + checkpoint);
-  checkpoint ++;
+  checkpoint++;
 }
 
-async function DeployUniswap(addresses, owner){
+async function DeployUniswap(addresses, owner) {
   const amountA = 1200000;
   const amountB = 1000000;
-  
-  await OccupyDAI(owner,amountA * 2);
+
+  await OccupyDAI(owner, amountA * 2);
 
   let checkpoint = 1
-  
-  
-  let hardhatDAI = await ethers.getContractAt('@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20', addresses["DAI"]);                
-  
 
-  
-  let hardhatEURs = await ethers.getContractAt('@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20', addresses["EURs"]);                
-  let hardhatsEUR = await ethers.getContractAt('@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20', addresses["sEUR"]);                
+
+  let hardhatDAI = await ethers.getContractAt('@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20', addresses["DAI"]);
+
+
+
+  let hardhatEURs = await ethers.getContractAt('@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20', addresses["EURs"]);
+  let hardhatsEUR = await ethers.getContractAt('@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20', addresses["sEUR"]);
 
 
   let uniConnector;
   let hardhatUniConnector;
-  
-  
+
+
 
   router = new ethers.ContractFactory(routerJson.abi, routerJson.bytecode, owner);
   hardhatRouter = await router.attach(addresses["UniRouter"]);
-  
- 
+
+
   uniConnector = await ethers.getContractFactory("UniswapConnector");
 
 
 
   //console.log( hardhatEURs.address  );
-  
+
   //EURs pool
   hardhatUniConnector = await uniConnector.deploy(
     addresses["UniFactory"],
@@ -290,7 +293,7 @@ async function DeployUniswap(addresses, owner){
   await hardhatUniConnector.deployed();
   addresses["UniswapConnectorEurS"] = hardhatUniConnector.address;
 
-//sEUR pool
+  //sEUR pool
   hardhatUniConnectorSeur = await uniConnector.deploy(
     addresses["UniFactory"],
     addresses["UniRouter"],
@@ -304,9 +307,9 @@ async function DeployUniswap(addresses, owner){
 
   /*
    * Add liquidity
-  */ 
+  */
 
-  
+
   /*console.log((await hardhatDAI.balanceOf(owner.address)).toNumber() );
   console.log((await hardhatEURs.balanceOf(owner.address)).toNumber() );
 */
@@ -314,16 +317,16 @@ async function DeployUniswap(addresses, owner){
 
   const deadline = Math.floor(Date.now() / 1000) + 120;
 
-/*  console.log("checkpoint " + checkpoint);
-  checkpoint ++;
-
-  console.log( await hardhatRouter.factory());
-*/
-
-    console.log(addresses);
-
-    console.log("checkpoint " + checkpoint);
+  /*  console.log("checkpoint " + checkpoint);
     checkpoint ++;
+  
+    console.log( await hardhatRouter.factory());
+  */
+
+  console.log(addresses);
+
+  console.log("checkpoint " + checkpoint);
+  checkpoint++;
 
   await hardhatDAI.connect(owner).approve(addresses["UniRouter"], amountA);
 
@@ -341,8 +344,8 @@ async function DeployUniswap(addresses, owner){
   );
 
   console.log("checkpoint " + checkpoint);
-  checkpoint ++;
-  
+  checkpoint++;
+
   await hardhatDAI.connect(owner).approve(addresses["UniRouter"], amountA);
 
   await hardhatsEUR.connect(owner).approve(addresses["UniRouter"], amountB);
@@ -359,9 +362,9 @@ async function DeployUniswap(addresses, owner){
   );
 
   console.log("checkpoint " + checkpoint);
-  checkpoint ++;
+  checkpoint++;
 
- const info = await hardhatUniConnector.pairInfo(hardhatDAI.address, hardhatEURs.address);
+  const info = await hardhatUniConnector.pairInfo(hardhatDAI.address, hardhatEURs.address);
 
   console.log(info[0].toNumber())
 }
@@ -421,3 +424,6 @@ main()
     console.error(error);
     process.exit(1);
   });
+
+
+  module.exports = { main }
---
+++ test/Fulltest/TestRedeemDeployed.js
@@ -0,0 +1,343 @@
+const { expect } = require("chai");
+const { waffle } = require("hardhat");
+const { utils } = require("ethers");
+const deploy = require("../../scripts/deployAll.js"); 
+
+describe("Swap Contract", function () {
+  let swapcontract;
+
+  let eurfix;
+  let usdfloat;
+  let dai;
+
+  let owner;
+  let saver;
+  let debtor;
+  let addrs;
+
+  let eurfix_amount;
+  let usdfloat_amount;
+
+  // test data. Use BigNumber to avoid overflow
+  const fullAmount = utils.parseEther("1000"); // 1000
+  const approvedAmount = utils.parseEther("200"); // 100
+  // => 100 EURFIX & 100 USDFLOAT are minted
+  const initialContractBalance = utils.parseEther("50"); // 100
+
+  beforeEach('Set up accounts', async () => {
+
+    await deploy.main();
+
+    // get addresses to interact
+    [owner, saver, debtor, ...addrs] = await ethers.getSigners();
+
+    await OccupyDAI(owner, fullAmount);
+
+    await main();
+  });
+
+  beforeEach('Deploy Contracts', async () => {
+    // main swap contract
+    const Swapcontract = await ethers.getContractFactory("SwapContract");
+    swapcontract = await Swapcontract.connect(owner).deploy();
+    await swapcontract.deployed();
+    // launch auxillary tokens and connect to main contract
+    const EURFIX = await ethers.getContractFactory("EURFIX");
+    eurfix = await EURFIX.connect(saver).deploy(swapcontract.address);
+    await eurfix.deployed();
+
+    const Usdfloat = await ethers.getContractFactory("USDFLOAT");
+    usdfloat = await Usdfloat.connect(debtor).deploy(swapcontract.address);
+    await usdfloat.deployed();
+
+    dai = await ethers.getContractAt('@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20', '0x6b175474e89094c44da98b954eedeac495271d0f');
+
+    // give derivative contract address to main address
+    await swapcontract.set_EURFIX_address(eurfix.address);
+    await swapcontract.set_USDFLOAT_address(usdfloat.address);
+    await swapcontract.set_Dai_address(dai.address);
+
+  });
+
+
+  beforeEach('Send initial Dai balance', async () => {
+    // send initial supply of Dai to the pool
+    await dai.transfer(swapcontract.address, initialContractBalance);
+
+  });
+  beforeEach('Initial transactions', async () => {
+    // send some initial Dai to the contract
+    await swapcontract.start_saving();
+    // start minting coins 
+    await dai.connect(owner).approve(swapcontract.address, approvedAmount);
+    const dai_allowance = await dai.allowance(owner.address, swapcontract.address);
+    await swapcontract.connect(owner).invest(approvedAmount);
+    eurfix_amount = await eurfix.balanceOf(owner.address);
+    usdfloat_amount = await usdfloat.balanceOf(owner.address);
+    //await dai.transfer(swapcontract.address, 5000000); // simulate some interest earned
+  });
+  beforeEach('Give allowance to burn EURFIX/usdfloat', async () => {
+    // send some initial Dai to the contract
+    await usdfloat.connect(owner).approve(swapcontract.address, approvedAmount.div(2));
+    await eurfix.connect(owner).approve(swapcontract.address, approvedAmount.div(2));
+  });
+
+  describe("Start the redeeming phase", function () {
+    it("Should allow Owner to start the redeeming phase", async function () {
+      // contract call
+      await swapcontract.start_redeeming();
+      const exchange_rate_end = await swapcontract.exchange_rate_end();
+
+      // should be sucessful
+      expect(exchange_rate_end).not.be.null;
+      expect(exchange_rate_end).to.equal(await swapcontract.getEUROPrice());
+      const principal_balance = await swapcontract.total_pool_prinicipal();
+      expect(principal_balance).to.equal(initialContractBalance.add(approvedAmount));
+
+    });
+    it("Should change the phase after start_redeeming() is called", async function () {
+      const old_phase = await swapcontract.current_phase();
+      //console.log("Before call the phase is", old_phase.toString());
+      expect(old_phase).to.equal(1);
+
+      await swapcontract.start_redeeming();
+
+      const new_phase = await swapcontract.current_phase();
+      //console.log("Before call the phase is", (await new_phase).toString());
+      expect(new_phase).to.equal(2);
+    });
+  });
+  describe("Should allow to redeem tokens", function () {
+    it("Should allow Whale to redeem both tokens", async function () {
+      // contract call
+      /*
+      console.log(
+        "Swap contract can spend up to ",
+        utils.formatEther((await usdfloat.allowance(owner.address, swapcontract.address)).toString()),
+        "USDFLOAT"
+      );
+      console.log(
+        "Swap contract can spend up to ",
+        utils.formatEther((await eurfix.allowance(owner.address, swapcontract.address)).toString()),
+        "EURFIX"
+      );
+      console.log(
+        "Owner has ",
+        utils.formatEther(await eurfix.balanceOf(owner.address)),
+        "EURFIX ")
+      console.log(
+        "Owner has ",
+        utils.formatEther(await usdfloat.balanceOf(owner.address)),
+        "USDFLOAT ")
+      */
+      console.log("This does not work with forks since aDAI balance returns 0");
+      await swapcontract.redeem(eurfix_amount, usdfloat_amount);
+      /*
+      console.log(
+        "Owner has ",
+        utils.formatEther(await eurfix.balanceOf(owner.address)),
+        "EURFIX and ",
+        utils.formatEther(await usdfloat.balanceOf(owner.address)),
+        "USDFLOAT");
+      console.log(
+        "Owner has ",
+        utils.formatEther(await dai.balanceOf(owner.address)),
+        "DAI ")
+      */
+    });
+    it("Should not allow Whale to redeem both with uneven numbers", async function () {
+      await expect(
+        swapcontract.redeem(eurfix_amount.sub("1"), usdfloat_amount)
+      ).to.be.revertedWith("Must exchange same amount of EURFIX and USDFLOAT");
+    });
+    it("Should allow person to redeem EURFIX at time", async function () {
+      await swapcontract.start_redeeming();
+      const dai_balance_saver_before = await dai.balanceOf(saver.address);
+      await eurfix.connect(owner).transfer(saver.address, eurfix_amount);
+      /*console.log(
+        "Swap has total of ",
+        utils.formatEther((await dai.balanceOf(swapcontract.address)).toString()),
+        "dai"
+      );*/
+      await eurfix.connect(saver).approve(swapcontract.address, eurfix_amount);
+      await swapcontract.connect(saver).redeem_EURFIX(eurfix_amount);
+      
+      const dai_balance_saver_after = await dai.balanceOf(saver.address);
+      expect(dai_balance_saver_after).to.be.above(dai_balance_saver_before);
+      /*console.log(
+        "Saver has total of  ",
+        utils.formatEther(dai_balance_saver_after),
+        "dai"
+      );*/
+      //console.log(dai_balance_saver.toString());
+      //console.log(eurfix_amount.toString());
+      expect(dai_balance_saver_after).to.be.at.least(eurfix_amount);
+    });
+    it("Should allow person to redeem USDFLOAT at time", async function () {
+      await swapcontract.start_redeeming();
+      const dai_balance_saver_before = await dai.balanceOf(saver.address);
+      await usdfloat.connect(owner).transfer(saver.address, usdfloat_amount);
+
+      await usdfloat.connect(saver).approve(swapcontract.address, usdfloat_amount);
+      await swapcontract.connect(saver).redeem_USDFLOAT(usdfloat_amount);
+      
+      const dai_balance_saver_after = await dai.balanceOf(saver.address);
+      expect(dai_balance_saver_after).to.be.above(dai_balance_saver_before);
+      expect(dai_balance_saver_after).to.be.at.least(usdfloat_amount);
+    });
+  });
+  describe("Should calculate correct payout factors", function () {
+    it("Should calculate the correct EURFIX payout factor given no change of exchange rate", async function () {
+      const exchange_rate = await swapcontract.getEUROPrice();
+      //console.log(
+      //  "Payout factor by current exchange rate of",
+      //  utils.formatUnits(
+      //    (exchange_rate).toString(),
+      //    unit = 8),
+      //  "is",
+      //  utils.formatUnits(
+      //    (await swapcontract.calculate_EURFIX_payout(exchange_rate)).toString(),
+      //    unit = 8)
+      //);
+      const EURFIX_payout_factor = await swapcontract.calculate_EURFIX_payout(exchange_rate);
+      //console.log(utils.parseUnits("1", unit=8));
+      //console.log(payout_factor);
+      expect(EURFIX_payout_factor).to.be.equal(utils.parseUnits("1", unit=8));
+    });
+    it("Should calculate the correct EURFIX payout factor given a change of exchange rate", async function () {
+      const exchange_rate = await swapcontract.getEUROPrice();
+      const new_exchange_rate = exchange_rate.mul("105").div("100"); // EURUSD increases by 5%
+      //console.log(
+      //  "Payout factor by current exchange rate of",
+      //  utils.formatUnits(
+      //    (exchange_rate).toString(),
+      //    unit = 8),
+      //  "is",
+      //  utils.formatUnits(
+      //    (await swapcontract.calculate_EURFIX_payout(exchange_rate)).toString(),
+      //    unit = 8)
+      //);
+      const EURFIX_payout_factor = await swapcontract.calculate_EURFIX_payout(new_exchange_rate);
+      //console.log(utils.parseUnits("1", unit=8));
+      //console.log(payout_factor);
+      expect(EURFIX_payout_factor).to.be.equal(utils.parseUnits("15", unit=7));
+    });
+    it("Should calculate the correct USDFLOAT payout factor given no change of exchange rate", async function () {
+      const exchange_rate = await swapcontract.getEUROPrice();
+      //console.log(
+      //  "Payout factor by current exchange rate of",
+      //  utils.formatUnits(
+      //    (exchange_rate).toString(),
+      //    unit = 8),
+      //  "is",
+      //  utils.formatUnits(
+      //    (await swapcontract.calculate_EURFIX_payout(exchange_rate)).toString(),
+      //    unit = 8)
+      //);
+      const USDFLOAT_payout_factor = await swapcontract.calculate_USDFLOAT_payout(exchange_rate);
+      //console.log(utils.parseUnits("1", unit=8));
+      //console.log(payout_factor);
+      expect(USDFLOAT_payout_factor).to.be.equal(utils.parseUnits("1", unit=8));
+    });
+    it("Should calculate the correct USDFLOAT payout factor given a change of exchange rate", async function () {
+      const exchange_rate = await swapcontract.getEUROPrice();
+      const new_exchange_rate = exchange_rate.mul("105").div("100"); // EURUSD increases by 5%
+      //console.log(
+      //  "Payout factor by current exchange rate of",
+      //  utils.formatUnits(
+      //    (exchange_rate).toString(),
+      //    unit = 8),
+      //  "is",
+      //  utils.formatUnits(
+      //    (await swapcontract.calculate_EURFIX_payout(exchange_rate)).toString(),
+      //    unit = 8)
+      //);
+      const USDFLOAT_payout_factor = await swapcontract.calculate_USDFLOAT_payout(new_exchange_rate);
+      //console.log(utils.parseUnits("1", unit=8));
+      //console.log(payout_factor);
+      expect(USDFLOAT_payout_factor).to.be.equal(utils.parseUnits("5", unit=7));
+    });
+  });
+  describe("Should redeem the correct amount of tokens", function () {
+    it("Should redeem the correct amount of EUFIX given a change of exchange rate", async function () {
+
+      // change exchange rate
+      const exchange_rate = await swapcontract.getEUROPrice();
+      const new_exchange_rate = exchange_rate.mul("101").div("100"); // EURUSD increases by 1%
+      
+      // calculate payout factor for new exchange rate
+      const EURFIX_payout_factor = await swapcontract.calculate_EURFIX_payout(new_exchange_rate);
+      
+      // calculate current value of certificates given payout factor
+      const payout = await swapcontract.EURFIX_to_Dai(eurfix_amount, EURFIX_payout_factor);
+
+      // 100 * (1 + 1% * leverage), where leverage = 10 
+      expect(payout).to.be.equal(utils.parseEther("110.0", unit=8));
+
+    });
+    it("Should redeem the correct amount of USDFLOAT given a change of exchange rate", async function () {
+
+      // change exchange rate
+      const exchange_rate = await swapcontract.getEUROPrice();
+      const new_exchange_rate = exchange_rate.mul("101").div("100"); // EURUSD increases by 1%
+      
+      // calculate payout factor for new exchange rate
+      const USDFLOAT_payout_factor = await swapcontract.calculate_USDFLOAT_payout(new_exchange_rate);
+      
+      // calculate current value of certificates given payout factor
+      const payout = await swapcontract.USDFLOAT_to_Dai(usdfloat_amount, USDFLOAT_payout_factor);
+
+      // 100 * (1 + 1% * leverage), where leverage = 10 
+      expect(payout).to.be.equal(utils.parseEther("90.0", unit=8));
+
+    });
+  });
+  const impersonateAddress = async (address) => {
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [address],
+    });
+
+    const signer = await ethers.provider.getSigner(address);
+    signer.address = signer._address;
+
+    return signer;
+  }
+
+  async function OccupyDAI(new_owner, transferAmount) {
+
+    let dai = await ethers.getContractAt('@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20', '0x6b175474e89094c44da98b954eedeac495271d0f');
+    const whale = '0x04ad0703b9c14a85a02920964f389973e094e153';
+    const whaleSigner = await impersonateAddress(whale);
+
+
+
+    let balance = await dai.balanceOf(whale);
+    /* console.log(
+       "whale balance (before) ",
+       utils.formatEther(balance)
+     );*/
+
+    balance = await dai.balanceOf(new_owner.address);
+    /*  console.log(
+        "our very own balance (before) ",
+        utils.formatEther(balance)
+      );*/
+
+    dai = dai.connect(whaleSigner);
+
+    await dai.transfer(new_owner.address, transferAmount);
+
+    balance = await dai.balanceOf(new_owner.address);
+
+    /*console.log(
+      "our very own balance (after) ",
+      utils.formatEther(balance)
+    )*/
+
+    return balance;
+
+
+  }
+
+});        
