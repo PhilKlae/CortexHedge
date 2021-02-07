6235e1d96303f0eb3565da55579bbc7a02a97466
PhilKlae <philipp.klaeser@t-online.de>
2021-02-07 19:35:39 +0100

    fix minor issues lel

--- scripts/deployAll.js
+++ scripts/deployAll.js
@@ -1,8 +1,8 @@
 const { expect } = require("ethers");
 //const { hexStripZeros } = require("ethers/lib/utils");
 const hre = require("hardhat");
-const routerJson = require('@uniswap/v2-periphery/build/UniswapV2Router02.json')
-
+const routerJson = require('@uniswap/v2-periphery/build/UniswapV2Router02.json');
+const ether = require("@openzeppelin/test-helpers/src/ether");
 
 
 
@@ -18,25 +18,25 @@ async function main() {
   if (hre.network.name == "hardhat") { //mainnet fork or local net 
 
     //Mainnet
-    addressDict["DAI"] = 0x6b175474e89094c44da98b954eedeac495271d0f;
-    addressDict["aDAI"] = 0xfc1e690f61efd961294b3e1ce3313fbd8aa4f85d;
-    addressDict["AaveLendingPoolProvider"] = 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5;
-    addressDict["Chainlink"] = 0xb49f677943BC038e9857d61E7d053CaA2C1734C1;
-    addressDict["UniFactory"] = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;//v2
-    addressDict["UniRouter"] = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
-    addressDict["Weth"] = 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2;
+    addressDict["DAI"] = "0x6b175474e89094c44da98b954eedeac495271d0f";
+    addressDict["aDAI"] = "0xfc1e690f61efd961294b3e1ce3313fbd8aa4f85d";
+    addressDict["AaveLendingPoolProvider"] = "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5";
+    addressDict["Chainlink"] = "0xb49f677943BC038e9857d61E7d053CaA2C1734C1";
+    addressDict["UniFactory"] = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";//v2
+    addressDict["UniRouter"] = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+    addressDict["Weth"] = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
 
 
   } else if (hre.network.name == "Kovan") {
 
     //KovanAddresses
-    addressDict["DAI"] = 0xff795577d9ac8bd7d90ee22b6c1703490b6512fd;
-    addressDict["aDAI"] = 0xdcf0af9e59c002fa3aa091a46196b37530fd48a8;
-    addressDict["AaveLendingPoolProvider"] = 0x88757f2f99175387ab4c6a4b3067c77a695b0349;
-    addressDict["Chainlink"] = 0x0c15Ab9A0DB086e062194c273CC79f41597Bbf13;//0xb49f677943BC038e9857d61E7d053CaA2C1734C1 mainnet
-    addressDict["UniFactory"] = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
-    addressDict["UniRouter"] = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
-    addressDict["Weth"] = 0xd0A1E359811322d97991E03f863a0C30C2cF029C;
+    addressDict["DAI"] = "0xff795577d9ac8bd7d90ee22b6c1703490b6512fd";
+    addressDict["aDAI"] = "0xdcf0af9e59c002fa3aa091a46196b37530fd48a8";
+    addressDict["AaveLendingPoolProvider"] = "0x88757f2f99175387ab4c6a4b3067c77a695b0349";
+    addressDict["Chainlink"] = "0x0c15Ab9A0DB086e062194c273CC79f41597Bbf13";//0xb49f677943BC038e9857d61E7d053CaA2C1734C1 mainnet
+    addressDict["UniFactory"] = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"; 
+    addressDict["UniRouter"] = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+    addressDict["Weth"] = "0xd0A1E359811322d97991E03f863a0C30C2cF029C";
 
   }
 
@@ -206,19 +206,43 @@ async function DeploySwapper( addresses, owner ) {
 }
 
 async function DeployUniswap(addresses, owner){
-  const amountDai = 12000000000;
-  const amountEURs = 10000000000;
+  const amountA = 12000000000;
+  const amountB = 10000000000;
+  
+  await OccupyDAI(owner,amountA);
+
+  let checkpoint = 1
+  
+  console.log("checkpoint " + checkpoint);
+  checkpoint ++;
   
   let hardhatDAI = await ethers.getContractAt('@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20', addresses["DAI"]);                
+  
+  console.log("checkpoint " + checkpoint);
+  checkpoint ++;
+  
   let hardhatEURs = await ethers.getContractAt('@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20', addresses["EURs"]);                
 
   let uniConnector;
   let hardhatUniConnector;
+  
+  console.log("checkpoint " + checkpoint);
+  checkpoint ++;
 
   router = new ethers.ContractFactory(routerJson.abi, routerJson.bytecode, owner);
   hardhatRouter = await router.attach(addresses["UniRouter"]);
   
+  console.log("checkpoint " + checkpoint);
+  checkpoint ++;
+
   uniConnector = await ethers.getContractFactory("UniswapConnector");
+
+  console.log("checkpoint " + checkpoint);
+  checkpoint ++;
+
+  //console.log( hardhatEURs.address  );
+  
+  
   hardhatUniConnector = await uniConnector.deploy(
     addresses["UniFactory"],
     addresses["UniRouter"],
@@ -226,31 +250,98 @@ async function DeployUniswap(addresses, owner){
     hardhatEURs.address
   );
 
+  console.log("checkpoint " + checkpoint);
+  checkpoint ++;
+
+  console.log("checkpoint " + checkpoint);
+  checkpoint ++; //7
   await hardhatUniConnector.deployed();
 
   /*
    * Add liquidity
   */ 
-  await hardhatDAI.approve(addressDict["UniRouter"], amountA);
+  await hardhatDAI.connect(owner).approve(addresses["UniRouter"], amountA);
 
-  await hardhatEURs.approve(addressDict["UniRouter"], amountB);
+  await hardhatEURs.connect(owner).approve(addresses["UniRouter"], amountB);
   
+  /*console.log((await hardhatDAI.balanceOf(owner.address)).toNumber() );
+  console.log((await hardhatEURs.balanceOf(owner.address)).toNumber() );
+*/
+
+
   const deadline = Math.floor(Date.now() / 1000) + 120;
+
+  console.log("checkpoint " + checkpoint);
+  checkpoint ++;
+
+  console.log( await hardhatRouter.factory());
 
   await hardhatRouter.addLiquidity(
     hardhatDAI.address,
-    hardhatEUR.address,
-    amountDAI,
-    amountEURs,
+    hardhatEURs.address,
+    amountA,
+    amountB,
     0,
     0,
     owner.address,
     deadline
   );
+  
+  console.log("checkpoint " + checkpoint);
+  checkpoint ++;
 
-  const info = await hardhatUniConnector.pairInfo(hardhatDAI.address, hardhatEUR.address);
+  const info = await hardhatUniConnector.pairInfo(hardhatDAI.address, hardhatEURs.address);
 
   console.log(info[0].toNumber())
+}
+
+
+const impersonateAddress = async (address) => {
+  await hre.network.provider.request({
+    method: "hardhat_impersonateAccount",
+    params: [address],
+  });
+
+  const signer = await ethers.provider.getSigner(address);
+  signer.address = signer._address;
+
+  return signer;
+}
+
+async function OccupyDAI(new_owner, transferAmount) {
+
+  let dai = await ethers.getContractAt('@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20', '0x6b175474e89094c44da98b954eedeac495271d0f');
+  const whale = '0x04ad0703b9c14a85a02920964f389973e094e153';
+  const whaleSigner = await impersonateAddress(whale);
+
+
+
+  let balance = await dai.balanceOf(whale);
+  /* console.log(
+     "whale balance (before) ",
+     utils.formatEther(balance)
+   );*/
+
+  balance = await dai.balanceOf(new_owner.address);
+  /*  console.log(
+      "our very own balance (before) ",
+      utils.formatEther(balance)
+    );*/
+
+  dai = dai.connect(whaleSigner);
+
+  await dai.transfer(new_owner.address, transferAmount);
+
+  balance = await dai.balanceOf(new_owner.address);
+
+  /*console.log(
+    "our very own balance (after) ",
+    utils.formatEther(balance)
+  )*/
+
+  return balance;
+
+
 }
 
 main()
